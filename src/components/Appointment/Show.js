43fb264815080d92854db867f53f4dd323acefec
Bita <bita.janzadeh@hotmail.com>
2022-01-20 00:38:42 -0500

    Create the Show component, import into stories index.js

---
+++ src/components/Appointment/Show.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Show(props) {
+
+  return ()
+}
+
+export default Show;
--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js"
 import Empty from "components/Appointment/Empty.js"
+import Show from "components/Appointment/Show.js"
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -143,3 +145,4 @@ storiesOf("Button", module)
           .add("Appointment with Time", () => <Appointment time="12pm" />)
           .add("Header", () => <Header time="12pm"/>)
           .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+
