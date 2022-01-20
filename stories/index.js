5ca3aae87db76a718239befbada0f61b2bdc8bea
Bita <bita.janzadeh@hotmail.com>
2022-01-20 01:36:43 -0500

    Create story to test Error component, update component to take in props being passed

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Empty from "components/Appointment/Empty.js"
 import Show from "components/Appointment/Show.js"
 import Confirm from "components/Appointment/Confirm.js"
 import Status from "components/Appointment/Status.js"
+import Error from "components/Appointment/Error.js"
 
 
 storiesOf("Button", module)
@@ -161,4 +162,9 @@ storiesOf("Button", module)
             onCancel={action("onCancel")} 
             />))
           .add("Status", () => <Status message="Deleting" />)
+          .add("Error", () => (
+          <Error 
+            message="Could not delete appointment." 
+            onClose={action("onClose")}
+          />))
               
