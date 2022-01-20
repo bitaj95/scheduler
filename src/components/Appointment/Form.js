3c586f565882a0c4f14329e55a7044723574d61a
Bita <bita.janzadeh@hotmail.com>
2022-01-20 02:27:47 -0500

    Add Story to test Form when in Edit and Create states

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
 
 function Form(props) {
 
@@ -23,8 +25,8 @@ function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger {/* your code goes here */}>Cancel</Button>
-          <Button confirm {/* your code goes here */}>Save</Button>
+          <Button danger >Cancel</Button>
+          <Button confirm >Save</Button>
         </section>
       </section>
     </main>
