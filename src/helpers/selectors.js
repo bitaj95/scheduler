52a7c3d0d80e6a93ac7d39f4ecc0a99d9fb903f8
Bita <bita.janzadeh@hotmail.com>
2022-01-21 17:41:17 -0500

    Create getAppointmentsForDay helper function with corresponding unit tests

---
+++ src/helpers/selectors.js
@@ -0,0 +1,14 @@
+export function getAppointmentsForDay(state, day) {
+  const filteredDays = state.days.filter( dayObj => dayObj.name === day ); 
+
+  //Return empty array if filteredDays array is empty
+  if (filteredDays.length === 0) {
+    return [];
+  }
+  
+  const appointmentsArray = filteredDays[0].appointments;
+  const results = []
+  appointmentsArray.forEach( appt =>  results.push(state.appointments[appt]))
+ 
+  return results;
+}
