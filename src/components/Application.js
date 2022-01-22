c88f681cc9efb6f42d6d52481507d8ddbe087e15
Bita <bita.janzadeh@hotmail.com>
2022-01-21 22:15:43 -0500

    Fix bug where student's name absent in Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -58,14 +58,14 @@ export default function Application(props) {
       axios.get(`http://localhost:8001/api/days`),
       axios.get(`http://localhost:8001/api/appointments`),
     ]).then((all) => {
-      console.log("ALL:", all[0].data);
+      console.log("Appointments:", all[1].data)
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
     });
   }, []);
 
   const setDay = day => setState({ ...state, day });
 
-  const dailyAppointments = getAppointmentsForDay( state ,state.day)
+  const dailyAppointments = getAppointmentsForDay( state, state.day)
 
   const appointmentsParsed = dailyAppointments.map( appointment => (
     <Appointment key={appointment.id} {...appointment} />
