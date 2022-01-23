bc0d1dc33b8f04355f3c7a02a950e3bf970ec962
Bita <bita.janzadeh@hotmail.com>
2022-01-22 21:09:43 -0500

    Add fake interviewers data and tests to confirm that the selector is functioning

--- src/components/Application.js
+++ src/components/Application.js
@@ -60,18 +60,23 @@ export default function Application(props) {
       axios.get(`http://localhost:8001/api/appointments`),
       axios.get(`http://localhost:8001/api/interviewers`),
     ]).then((all) => {
-      console.log("Appointments:", all[1].data)
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     });
   }, []);
+
+  console.log("**State interviewers check:", state.interviewers);
 
   const setDay = day => setState({ ...state, day });
 
   const dailyAppointments = getAppointmentsForDay( state, state.day)
 
-  const appointmentsParsed = dailyAppointments.map( appointment => (
-    <Appointment key={appointment.id} {...appointment} />
-    ))
+  const appointmentsParsed = dailyAppointments.map( appointment => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment key={appointment.id} {...appointment} interview={interview}  />
+    )
+  })
     
   //const setDays = (days) => setState(prev => ({ ...prev, days }));
   /*   useEffect( () => {
