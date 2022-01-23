6e9bd341b6b9439781467c64fed1430ed245e246
Bita <bita.janzadeh@hotmail.com>
2022-01-23 00:37:16 -0500

    Create transition function in useVisualMode which updates mode state to a new value

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,20 @@
-export function useVisualMode(initial) {
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-  return {
-    mode
+  const transition = (newMode) => {
+    setMode(newMode);
   }
+
+  return { mode, transition }
 }
+
+/* 
+export default function useTransition(previousMode, newMode) {
+  const previousObj = useVisualMode(previousMode);
+  const updatedObj = useVisualMode(newMode);
+
+  return {...previousObj, ...updatedObj };
+
+} */
