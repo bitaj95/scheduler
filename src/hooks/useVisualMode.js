4c88fd93d5fde5d0897f174ef2e2528f114718ac
Bita <bita.janzadeh@hotmail.com>
2022-01-23 01:59:22 -0500

    Implement back feature:  transition back to our previous mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,19 +2,22 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   const transition = (newMode) => {
     setMode(newMode);
+    setHistory([...history, newMode])
   }
 
-  return { mode, transition }
+  const back = () => {
+    if (history.length > 1 ) {
+      const historyGoneBack = history.slice(0, -1);
+      setHistory(history.slice(0, -1));
+      setMode(historyGoneBack.slice(-1)[0]);
+    }
+
+  
+  }
+
+  return { mode, transition, back }
 }
-
-/* 
-export default function useTransition(previousMode, newMode) {
-  const previousObj = useVisualMode(previousMode);
-  const updatedObj = useVisualMode(newMode);
-
-  return {...previousObj, ...updatedObj };
-
-} */
