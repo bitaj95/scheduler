d469810fc9960733deb268f4a2665e3b4b0099e4
Bita <bita.janzadeh@hotmail.com>
2022-01-25 23:55:50 -0500

    Update number of spots remaining in a day when we add or remove an appointment

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -9,7 +9,7 @@ export default function useApplicationData() {
     appointments: {}, 
     interviewers: {}
   });
-  
+
   useEffect( () => {
     Promise.all([
       axios.get(`http://localhost:8001/api/days`),
@@ -19,11 +19,10 @@ export default function useApplicationData() {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     });
   }, []);
-  
+
   const setDay = day => setState({ ...state, day });
   
   function bookInterview(id, interview) {
-    console.log(id, interview);
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -32,12 +31,32 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
+
+        /*   state.days contains array of all the 5 days objects (Mon - Fri), the key "name" holds day name
+    state.day contains the name of the day of appointments being viewed 
+    --> targetedDay contains the days object (from state.days) of the day user selected */
+    const targetedDay = state.days.filter( day => day.name === state.day );
+    //Locate index in "days" array where the selected day is located
+    const dayIndex = state.days.findIndex(day => day.name === state.day)
+  
+    const dayWithUpdatedSpots = {
+      ...targetedDay[0],
+      spots: targetedDay[0].spots - 1 
+    }
+    // Created updated version of entire days object to display new # spots
+    const days = [
+      ...state.days.slice(0, dayIndex),
+      dayWithUpdatedSpots,
+      ...state.days.slice(dayIndex + 1)
+    ]
+
     return axios
     .put(`http://localhost:8001/api/appointments/${id}`, { interview })
     .then( (res) => {
       setState({
         ...state,
-        appointments
+        appointments,
+        days
       });
      })
   }
@@ -51,12 +70,31 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
+
+    /*   state.days contains array of all the 5 days objects (Mon - Fri), the key "name" holds day name
+    state.day contains the name of the day of appointments being viewed 
+    --> targetedDay will hold the days object (from state.days) of the day user selected */
+    const targetedDay = state.days.filter( day => day.name === state.day );
+    const dayIndex = state.days.findIndex(day => day.name === state.day)
+  
+    const dayWithUpdatedSpots = {
+      ...targetedDay[0],
+      spots: targetedDay[0].spots + 1 
+    }
+    // Created updated version of entire days object to display new # spots
+    const days = [
+      ...state.days.slice(0, dayIndex),
+      dayWithUpdatedSpots,
+      ...state.days.slice(dayIndex + 1)
+    ]
+
     return axios
     .delete(`/api/appointments/${id}`)
     .then( (res) => {
       setState({
         ...state,
-        appointments
+        appointments,
+        days
         }
       )
     })
