619b79ee4425a3d3267c7b0ab52c1d89b958fda5
Bita <bita.janzadeh@hotmail.com>
2022-01-26 16:37:40 -0500

    Implement validation that student name has been entered in form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,16 @@ function Form(props) {
 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(student, interviewer);
+  }
 
   const reset = () => {
     setStudent("")
@@ -31,6 +41,7 @@ function Form(props) {
             data-testid={'student-name-input'}
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           interviewers = {props.interviewers}
           value={interviewer}
@@ -40,7 +51,7 @@ function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel} > Cancel</Button>
-          <Button confirm onClick={ () => props.onSave(student, interviewer)} >Save</Button>
+          <Button confirm onClick={validate} >Save</Button>
         </section>
       </section>
     </main>
